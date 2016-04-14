79eacaa57f25775c94dc5588038fffd94b1420c6
Rohtash Singh <rohtash.singh@gmail.com>
2016-04-14 11:17:40 -0700

    Updated the root file and build the code.

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
-dCore
-=======
+Apparatus
+=========
 
 This repository contains the java core project.
 
-The base project which this reppository handles is the dCore project.
+The base project which this repository handles is the Apparatus project.
 
-The dCore project should have the basic and core implementations which can be used by other projects. 
-It is an independant project and should not depend on any other library already available for reuse.
+The Apparatus project should have the basic and core implementations which can be used by other projects. 
+It is an independent project and should not depend on any other library already available for reuse.
 
-
-
+The source code available in this repository will be available for AS IT IS usage.
--- src/com/apparatus/Constants.java
+++ src/com/apparatus/Constants.java
@@ -1,36 +1,32 @@
 /******************************************************************************
  * Copyright (C) Devamatre/Devmatre Inc. 2009
  * 
- * This code is licensed to Devamatre under one or more contributor license 
- * agreements. The reproduction, transmission or use of this code or the 
- * snippet is not permitted without prior express written consent of Devamatre. 
+ * This code is licensed to Devamatre under one or more contributor license
+ * agreements. The reproduction, transmission or use of this code or the
+ * snippet is not permitted without prior express written consent of Devamatre.
  * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the license is distributed on an "AS IS" BASIS, WITHOUT 
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied and the 
- * offenders will be liable for any damages. All rights, including  but not
- * limited to rights created by patent grant or registration of a utility model 
- * or design, are reserved. Technical specifications and features are binding 
- * only insofar as they are specifically and expressly agreed upon in a written 
+ * distributed under the license is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied and the
+ * offenders will be liable for any damages. All rights, including but not
+ * limited to rights created by patent grant or registration of a utility model
+ * or design, are reserved. Technical specifications and features are binding
+ * only insofar as they are specifically and expressly agreed upon in a written
  * contract.
  * 
  * You may obtain a copy of the License for more details at:
- *      http://www.devamatre.com/licenses/license.txt.
- *      
- * Devamatre reserves the right to modify the technical specifications and or 
+ * http://www.devamatre.com/licenses/license.txt.
+ * 
+ * Devamatre reserves the right to modify the technical specifications and or
  * features without any prior notice.
  *****************************************************************************/
 package com.apparatus;
 
 /**
- * Constants.java
+ * The <code>Constants</code> class defines the constants used in this project.
  * 
- * The <code>Constants</code> 
- * TODO Define Purpose here
- * 
- * 
-  * @author Rohtash Singh (rohtash.singh@gmail.com)
- * @created 01/12/2012 (dd/mm/yyyy)
+ * @author Rohtash Singh Lakra (rohtash.singh@gmail.com)
+ * @created Dec 01, 2012 11:14:03 PM
  * @version 1.0.0
  * @since 1.0.0
  */
@@ -39,25 +35,24 @@ public interface Constants {
 	boolean TRUE = true;
 	boolean FALSE = false;
 	
-    /* SQL Server Driver Details */
+	/* SQL Server Driver Details */
 	String DRIVER_PREFIX = "jdbc:odbc:";
 	
-    /* JDBC */
-    String JDBC_DRIVER_CLASS = "sun.jdbc.odbc.JdbcOdbcDriver";
-    String DSN_NAME = "jdbc:odbc:BankDB";
-
-    /* System Details */
-    String HOST = "192.168.0.43";
-    String SQLSERVER_PORT = "1433"; //Default Port for SQL Server
-
-    /* User Details */
-    String USERNAME = "sa";
-    String PASSWORD = "hrhk";
-
-    /* SQL Server */
-    String SQL_SERVER_DRIVER = 	"com.ddtek.jdbc.sqlserver.SQLServerDriver";
-    String SQL_DRIVER_VENDER_NAME = "jdbc:datadirect:sqlserver";
-    String DATABASE_NAME = "Vines";
-    String ConnectionURL = SQL_DRIVER_VENDER_NAME + "://" + HOST + ":"  + 
-    	SQLSERVER_PORT + ";DatabaseName=" + DATABASE_NAME;
+	/* JDBC */
+	String JDBC_DRIVER_CLASS = "sun.jdbc.odbc.JdbcOdbcDriver";
+	String DSN_NAME = "jdbc:odbc:BankDB";
+	
+	/* System Details */
+	String HOST = "192.168.0.43";
+	String SQLSERVER_PORT = "1433"; // Default Port for SQL Server
+	
+	/* User Details */
+	String USERNAME = "sa";
+	String PASSWORD = "hrhk";
+	
+	/* SQL Server */
+	String SQL_SERVER_DRIVER = "com.ddtek.jdbc.sqlserver.SQLServerDriver";
+	String SQL_DRIVER_VENDER_NAME = "jdbc:datadirect:sqlserver";
+	String DATABASE_NAME = "Vines";
+	String ConnectionURL = SQL_DRIVER_VENDER_NAME + "://" + HOST + ":" + SQLSERVER_PORT + ";DatabaseName=" + DATABASE_NAME;
 }
