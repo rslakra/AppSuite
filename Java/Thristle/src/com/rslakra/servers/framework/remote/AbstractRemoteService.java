c111f16352d688322acae76003459d72e49698b4
Rohtash Singh Lakra <rslakra@users.noreply.github.com>
2016-06-12 17:28:56 -0700

    Added new remote service.

---
+++ Java/Thristle/src/com/rslakra/servers/framework/remote/AbstractRemoteService.java
@@ -0,0 +1,10 @@
+package com.rslakra.servers.framework.remote;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AbstractRemoteService {
+	protected HttpServletRequest servletRequest;
+	protected HttpServletResponse servletResponse;
+	
+}
