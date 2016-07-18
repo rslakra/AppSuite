a710283f928cc4771f95de2ff2bd2ab5a39ffcb4
Rohtash Singh Lakra <rslakra@users.noreply.github.com>
2016-07-18 16:31:03 -0700

    Removed unwanted code.

--- Java/Apparatus/src/com/apparatus/interviews/ListPalindromes.java
+++ Java/Apparatus/src/com/apparatus/interviews/ListPalindromes.java
@@ -1,21 +1,12 @@
 package com.apparatus.interviews;
 
 import java.io.BufferedReader;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.TreeMap;
 
 public class ListPalindromes {
-	
-	static Map<String, Integer> initialMap = new LinkedHashMap<String, Integer>();
-	public static boolean ASC = true;
-	public static boolean DESC = false;
 	
 	static Map<String, Integer> map = new HashMap<String, Integer>();
 	static MapValueComparator valueComparator = new MapValueComparator(map);
@@ -49,22 +40,18 @@ public class ListPalindromes {
 			System.out.println("Error while reading file line by line");
 		}
 		
-		Map<String, Integer> sortedMapAsc = sortByComparator(initialMap, DESC);
-		printMap(sortedMapAsc);
-		
 		System.out.println("unsorted map:" + map);
 		mapSorted.putAll(map);
 		System.out.println("mapSorted:" + mapSorted);
 	}
 	
 	public void add(String str) {
-		Integer counter = initialMap.get(str);
+		Integer counter = map.get(str);
 		if(counter == null) {
 			counter = 0;
 		}
 		
 		counter++;
-		initialMap.put(str, counter);
 		map.put(str, counter);
 	}
 	
@@ -75,7 +62,7 @@ public class ListPalindromes {
 		
 		for(int index = 0; index < words.length; index++) {
 			String word = words[index].toLowerCase();
-			if(initialMap.containsKey(word)) {
+			if(map.containsKey(word)) {
 				this.add(word);
 			} else if(isPalindrome(word)) {
 				this.add(word);
@@ -84,7 +71,7 @@ public class ListPalindromes {
 	}
 	
 	public static boolean isPalindrome(String wordToBeChecked) {
-		boolean isPalindrome = true;
+		boolean isPalindrome = (wordToBeChecked != null);
 		if(wordToBeChecked != null && wordToBeChecked.length() > 0) {
 			int wordLength = wordToBeChecked.length();
 			for(int index = 0; index < wordLength; index++) {
@@ -98,38 +85,13 @@ public class ListPalindromes {
 		return isPalindrome;
 	}
 	
-	private static Map<String, Integer> sortByComparator(Map<String, Integer> unsortMap, final boolean order) {
-		System.out.println("Testing palindromes");
-		List<Entry<String, Integer>> list = new LinkedList<Entry<String, Integer>>(unsortMap.entrySet());
-		
-		// Sorting the list based on values
-		Collections.sort(list, new Comparator<Entry<String, Integer>>() {
-			public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
-				if(order) {
-					return o1.getValue().compareTo(o2.getValue());
-				} else {
-					return o2.getValue().compareTo(o1.getValue());
-					
-				}
-			}
-		});
-		
-		Map<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
-		for(Entry<String, Integer> entry : list) {
-			sortedMap.put(entry.getKey(), entry.getValue());
-		}
-		
-		return sortedMap;
-	}
-	
-	public static void printMap(Map<String, Integer> map) {
-		for(Entry<String, Integer> entry : map.entrySet()) {
-			System.out.println(entry.getKey() + " > " + entry.getValue());
-		}
-	}
 }
 
-
+/**
+ * 
+ * @author singhr
+ *
+ */
 class MapValueComparator implements Comparator<String> {
 	Map<String, Integer> mapToSort;
 	
@@ -141,10 +103,10 @@ class MapValueComparator implements Comparator<String> {
 	 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
 	 */
 	public int compare(String entry, String newEntry) {
-//		return (mapToSort.get(entry) - mapToSort.get(newEntry));
+		// return (mapToSort.get(entry) - mapToSort.get(newEntry));
 		if(mapToSort.get(entry) > mapToSort.get(newEntry)) {
 			return 1;
-		}else if(mapToSort.get(entry) < mapToSort.get(newEntry)) {
+		} else if(mapToSort.get(entry) < mapToSort.get(newEntry)) {
 			return -1;
 		} else {
 			return (entry.compareTo(newEntry) > 0 ? 1 : -1);
