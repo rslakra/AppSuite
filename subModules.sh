2a622e4c162dd2c3e113a0aafbd63f6870338ee4
Rohtash Lakra <rslakra@users.noreply.github.com>
2023-11-11 23:29:12 -0800

    updated submodules script to sync and checkout master branch

--- subModules.sh
+++ subModules.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 #Author:Rohtash Lakra
 clear
-# Step 1
+# Step 1 - initialize submodules recursively
 git submodule update --init --recursive
-# Step 2
+# Step 2 - - pull submodules recursively
 git pull --recurse-submodules
 echo
-
+# Step 3 - checkout master branch of submodules recursively (instead of #head branch)
+PROJECT_HOME=$PWD
+echo
+echo "Syncing [${PROJECT_HOME}] ..."
+echo
+echo
+# use for loop to read all values and indexes
+for dir in $(pwd)/*/;
+do
+  dirName=$(basename "${dir}");
+  echo "Syncing [[${dirName}] ..."
+  if [[ ${dirName} != "Infra" ]] ; then
+    cd ${dirName}
+    git checkout master
+    cd ..
+  fi
+  echo
+done
+echo
