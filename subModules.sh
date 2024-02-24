79e0303300ef5f4f1459c7202632aeee7eb03f0f
Rohtash Lakra <rslakra@users.noreply.github.com>
2024-02-23 22:26:58 -0800

    merged local changes and updated appsuite-core version

--- subModules.sh
+++ subModules.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 #Author:Rohtash Lakra
 clear
+echo
 # Step 1 - initialize submodules recursively
 git submodule update --init --recursive
+echo
+
 # Step 2 - - pull submodules recursively
 git pull --recurse-submodules
 echo
+
 # Step 3 - checkout master branch of submodules recursively (instead of #head branch)
 PROJECT_HOME=$PWD
 echo
@@ -16,7 +20,7 @@ echo
 for dir in $(pwd)/*/;
 do
   dirName=$(basename "${dir}");
-  echo "Syncing [[${dirName}] ..."
+  echo "Syncing [${dirName}] ..."
   if [[ ${dirName} != "Infra" ]] ; then
     cd ${dirName}
     git checkout master
